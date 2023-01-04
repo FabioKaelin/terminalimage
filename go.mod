27a3090290403592e1d7f686c22ac64d2544c7e4
FabioKaelin <fabio_kaelin@swissre.com>
2023-01-04 11:14:30 +0100

    terminalimage: changes for v0.1.0

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/fabiokaelin/terminalimage
+
+go 1.19
+
+require (
+	github.com/gookit/color v1.5.2
+	golang.org/x/image v0.2.0
+)
+
+require (
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/sys v0.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"strings"
+
+	"os"
+
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+
+	"github.com/gookit/color"
+	_ "golang.org/x/image/webp"
+)
+
+func main() {
+	str, err := ImageToString("img.jfif", 20, true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(str)
+}
+
+func ImgDataToArray(imageData image.Image, height int, pixelSplit bool) ([]string, error) {
+	maxX := imageData.Bounds().Max.X
+	maxY := imageData.Bounds().Max.Y
+	YBlockSize := maxY / height
+	width := maxX / YBlockSize
+	XBlockSize := maxX / width
+	var result []string
+	for YBlockNr := 0; YBlockNr < height; YBlockNr++ {
+		lineResult := ""
+		if pixelSplit {
+			for XBlockNr := 0; XBlockNr < width*2; XBlockNr++ {
+				sumR := 0
+				sumG := 0
+				sumB := 0
+				for XPixelNr := 0; XPixelNr < XBlockSize/2; XPixelNr++ {
+					for YPixelNr := 0; YPixelNr < YBlockSize; YPixelNr++ {
+						r, g, b, _ := imageData.At(XPixelNr+int(float64(XBlockNr)/float64(2)*float64(XBlockSize)), YPixelNr+(YBlockNr*(YBlockSize))).RGBA()
+						sumR += int(r / 256)
+						sumG += int(g / 256)
+						sumB += int(b / 256)
+					}
+				}
+				lineResult += color.Sprintf("<fg=%d,%d,%d;bg=%d,%d,%d>█</>", int(sumR/((XBlockSize/2)*(YBlockSize))), int(sumG/((XBlockSize/2)*(YBlockSize))), int(sumB/((XBlockSize/2)*(YBlockSize))), int(sumR/((XBlockSize/2)*(YBlockSize))), int(sumG/((XBlockSize/2)*(YBlockSize))), int(sumB/((XBlockSize/2)*(YBlockSize))))
+			}
+		} else {
+			for XBlockNr := 0; XBlockNr < width; XBlockNr++ {
+				sumR := 0
+				sumG := 0
+				sumB := 0
+				for XPixelNr := 0; XPixelNr < XBlockSize; XPixelNr++ {
+					for YPixelNr := 0; YPixelNr < YBlockSize; YPixelNr++ {
+						r, g, b, _ := imageData.At(XPixelNr+(XBlockNr*XBlockSize), YPixelNr+(YBlockNr*YBlockSize)).RGBA()
+						sumR += int(r / 256)
+						sumG += int(g / 256)
+						sumB += int(b / 256)
+					}
+				}
+				lineResult += color.Sprintf("<fg=%d,%d,%d;bg=%d,%d,%d>██</>", int(sumR/(XBlockSize*YBlockSize)), int(sumG/(XBlockSize*YBlockSize)), int(sumB/(XBlockSize*YBlockSize)), int(sumR/(XBlockSize*YBlockSize)), int(sumG/(XBlockSize*YBlockSize)), int(sumB/(XBlockSize*YBlockSize)))
+
+			}
+		}
+		result = append(result, lineResult)
+	}
+	return result, nil
+}
+
+func ImageToString(path string, height int, pixelSplit bool) (string, error) {
+
+	existingImageFile, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+
+	defer existingImageFile.Close()
+
+	existingImageFile.Seek(0, 0)
+
+	imageData, _, err := image.Decode(existingImageFile)
+	if err != nil {
+		return "", err
+	}
+	resultArray, err := ImgDataToArray(imageData, height, pixelSplit)
+	if err != nil {
+		return "", err
+	}
+	return strings.Join(resultArray[:], "\n"), nil
+}
