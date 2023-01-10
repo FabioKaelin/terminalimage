d795713674f7ada2068283863e1ee3e572b23694
FabioKaelin <fabio_kaelin@swissre.com>
2023-01-10 08:36:10 +0100

    fix image is cut

--- terminalimage.go
+++ terminalimage.go
@@ -36,35 +36,39 @@ func Example() {
 func ImgDataToArray(imageData image.Image, height int, pixelSplit bool) ([]string, error) {
 	maxX := imageData.Bounds().Max.X
 	maxY := imageData.Bounds().Max.Y
-	YBlockSize := maxY / height
-	width := maxX / YBlockSize
-	XBlockSize := maxX / width
+	YBlockSize := int(float64(maxY) / float64(height))
+	width := int(float64(maxX) / (float64(maxY) / float64(height)))
+	XBlockSize := int(float64(maxX) / (float64(maxX) / (float64(maxY) / float64(height))))
 	var result []string
-	PixelSize := 1
+	Dividor := 1
 	if pixelSplit {
-		PixelSize = 2
+		Dividor = 2
 	}
 	for YBlockNr := 0; YBlockNr < height; YBlockNr++ {
-		result = append(result, blockLine(imageData, PixelSize, width, XBlockSize, YBlockSize, YBlockNr))
+		result = append(result, blockLine(imageData, Dividor, width, XBlockSize, YBlockSize, YBlockNr))
 	}
 	return result, nil
 }
 
-func blockLine(imageData image.Image, PixelSize int, width int, XBlockSize int, YBlockSize int, YBlockNr int) string {
+func blockLine(imageData image.Image, Dividor int, width int, XBlockSize int, YBlockSize int, YBlockNr int) string {
 	lineResult := ""
-	for XBlockNr := 0; XBlockNr < width*PixelSize; XBlockNr++ {
+	PixelSize := 2
+	if Dividor == 2 {
+		PixelSize = 1
+	}
+	for XBlockNr := 0; XBlockNr < width*Dividor; XBlockNr++ {
 		sumR := 0
 		sumG := 0
 		sumB := 0
-		for XPixelNr := 0; XPixelNr < XBlockSize/PixelSize; XPixelNr++ {
+		for XPixelNr := 0; XPixelNr < XBlockSize/Dividor; XPixelNr++ {
 			for YPixelNr := 0; YPixelNr < YBlockSize; YPixelNr++ {
-				r, g, b, _ := imageData.At(XPixelNr+int(float64(XBlockNr)/float64(PixelSize)*float64(XBlockSize)), int(float64(YPixelNr)+(float64(YBlockNr)*(float64(YBlockSize))))).RGBA()
+				r, g, b, _ := imageData.At(XPixelNr+int(float64(XBlockNr)/float64(Dividor)*float64(XBlockSize)), int(float64(YPixelNr)+(float64(YBlockNr)*(float64(YBlockSize))))).RGBA()
 				sumR += int(r / 256)
 				sumG += int(g / 256)
 				sumB += int(b / 256)
 			}
 		}
-		lineResult += color.Sprintf("<fg=%d,%d,%d;bg=%d,%d,%d>%s</>", int(float64(sumR)/((float64(XBlockSize)/float64(PixelSize))*(float64(YBlockSize)))), int(float64(sumG)/((float64(XBlockSize)/float64(PixelSize))*(float64(YBlockSize)))), int(float64(sumB)/((float64(XBlockSize)/float64(PixelSize))*(float64(YBlockSize)))), int(float64(sumR)/((float64(XBlockSize)/float64(PixelSize))*(float64(YBlockSize)))), int(float64(sumG)/((float64(XBlockSize)/float64(PixelSize))*(float64(YBlockSize)))), int(float64(sumB)/((float64(XBlockSize)/float64(PixelSize))*(float64(YBlockSize)))), strings.Repeat("█", PixelSize))
+		lineResult += color.Sprintf("<fg=%d,%d,%d;bg=%d,%d,%d>%s</>", int(float64(sumR)/((float64(XBlockSize)/float64(Dividor))*(float64(YBlockSize)))), int(float64(sumG)/((float64(XBlockSize)/float64(Dividor))*(float64(YBlockSize)))), int(float64(sumB)/((float64(XBlockSize)/float64(Dividor))*(float64(YBlockSize)))), int(float64(sumR)/((float64(XBlockSize)/float64(Dividor))*(float64(YBlockSize)))), int(float64(sumG)/((float64(XBlockSize)/float64(Dividor))*(float64(YBlockSize)))), int(float64(sumB)/((float64(XBlockSize)/float64(Dividor))*(float64(YBlockSize)))), strings.Repeat("█", PixelSize))
 	}
 	return lineResult
 }
